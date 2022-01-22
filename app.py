f6e97926c26ee49bf9c58f22396dc023fd0165d9
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-01-22 11:00:20 +0100

    Add files via upload
    Ratelimit: 60 seconds per 1 request

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+from typing import Union
+
+
+app = Flask(__name__)
+
+@app.route("/api/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/api/check/<domain>', timeout=10.0)
+def check(domain):
+    r = requests.get(url=f'https://{domain}')
+    status_code = r.status_code 
+    if status_code == 200:
+        return status_code
+    elif status_code != 200:
+        return status_code
+    
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
