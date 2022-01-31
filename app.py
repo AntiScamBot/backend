e31f6f8a06b1a9c97871fe5c9b475aa514501f03
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-01-31 09:38:19 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask
 import requests
 
+
 app = Flask(__name__)
 
 @app.route("/api/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>API for the AntiScam Bot.</p>"
 
 
-@app.route('/api/check/<domain>', timeout=10.0)
+@app.route('/api/check/<domain>', methods=["GET"])
 def check(domain):
+    # Ratelimit: 60 seconds per 1 request
     r = requests.get(url=f'https://{domain}')
     status_code = r.status_code 
     if status_code == 200:
         return status_code
     elif status_code != 200:
         return status_code
-    
-    
     
 if __name__ == '__main__':
     app.run(debug=True)
