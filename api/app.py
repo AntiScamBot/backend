a3854041370422acb94c6b2c8d6f2aabedfb1f6d
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-05 14:09:38 +0100

    Add files via upload
    sorry, it was a mistake

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import requests
+import urllib
 
 """__all__ = (
     'check',
@@ -14,10 +14,15 @@ def api():
 
 @app.route('/api/check/<domain>', methods=["GET"])
 def check(domain):
+    response = urllib.request.urlopen(f'https://{domain}')
+    status_code = response.getcode()
+    if status_code:
+        return jsonify(code=status_code)
+    """
     r = requests.get(url=f'https://{domain}').status_code
     if r:
         return jsonify(code=r)
-
+"""
     
 if __name__ == '__main__':
     app.run(debug=False)
