aa8a466070573d8d8e0731811f2f420534999688
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-05 11:49:52 +0100

    Create test_package.py

---
+++ tests/test_package.py
@@ -0,0 +1,26 @@
+import pytest
+from app import app
+
+# Dummy datas
+DUMMY_DOMAIN = "example.com"
+
+class TestEverything:
+    async def check_client(self):
+        pass
+
+    @pytest.fixture
+    def client():
+        with app.test_client() as client:
+            yield client
+        
+    def test_statuscode(client):
+        """
+        Tests /api/check API: Correct credentials
+        """
+        response = client.post(f'/api/check/{DUMMY_DOMAIN}')
+        return response.text()
+
+    @pytest.mark.asyncio
+    async def pytest_sessionfinish(self):
+        """Closes the session at the end of the tests"""
+        await pytest.exit("succesfull exited")
