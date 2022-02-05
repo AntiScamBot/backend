931021b7aa6c9177b09fb8a2681d9e3bcf7df5a3
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-05 13:10:49 +0100

    Create app.py

---
+++ api/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/api/")
+def hello_world():
+    return "<p>API for the AntiScam Bot.</p>"
+
+
+@app.route('/api/check/<domain>')
+def check(domain):
+    r = requests.get(url=f'https://{domain}')
+    status_code = r.status_code 
+    if status_code:
+        return jsonify(code={status_code})
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
